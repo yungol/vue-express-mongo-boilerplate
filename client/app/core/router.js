7c1b2118937580dcd9c92b02d7eda899bd184516
fakass <jdjara@gmail.com>
2017-10-12 15:41:01 -0500

    se crea otro modulo y se organiza la sidebar para que resalte segun la ruta en la que esta

--- client/app/core/router.js
+++ client/app/core/router.js
@@ -8,17 +8,19 @@ import Counter from "../modules/counter";
 import Devices from "../modules/devices";
 import Posts from "../modules/posts";
 import Profile from "../modules/profile";
+import Diagnostico from "../modules/diagnostico";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
 	mode: "hash",
 	routes: [
-		{ path: "/", component: Home },
-		{ path: "/devices", component: Devices },
-		{ path: "/posts", component: Posts },
-		{ path: "/counter", component: Counter },	
-		{ path: "/profile", component: Profile }
+		{ path: "/", component: Home, name: 'inicio' },
+		{ path: "/devices", component: Devices, name:'devices' },
+		{ path: "/posts", component: Posts, name: 'posts' },
+		{ path: "/counter", component: Counter, name: 'counter' },	
+		{ path: "/profile", component: Profile, name: 'profile' },
+		{ path: "/diagnostico", component: Diagnostico, name: 'diagnostico' }
 		// { path: "/users", component: User, meta: { needRole: "admin" } },
 		//{ path: "*", component: NotFound }
 	]
