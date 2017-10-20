2f3bf10eed8a14def344cc0539768823a9f85a1c
yungol <jdjara@gmail.com>
2017-10-20 08:48:14 -0500

    se hace build

--- server/public/app/frontend.js
+++ server/public/app/frontend.js
@@ -1,1 +1,1 @@
-webpackJsonp([1],{205:function(n,o,e){"use strict";var t=e(295);!function(n){n&&n.__esModule}(t);console.log("Frontend loaded!")},295:function(n,o){},412:function(n,o,e){n.exports=e(205)}},[412]);
+webpackJsonp([1],{207:function(n,o,e){"use strict";var t=e(295);!function(n){n&&n.__esModule}(t);console.log("Frontend loaded!")},295:function(n,o){},404:function(n,o,e){n.exports=e(207)}},[404]);
