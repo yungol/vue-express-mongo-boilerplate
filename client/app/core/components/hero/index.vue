62f76a31a742dc80e3c715ea5d34ed328bca2e47
yungol <jdjara@gmail.com>
2017-10-20 11:31:21 -0500

    el hero cambia con las rutas

--- client/app/core/components/hero/index.vue
+++ client/app/core/components/hero/index.vue
@@ -2,14 +2,42 @@
     section.hero.is-dark
         .hero-body
             h1.title
-                | Este es el titulo
+                | {{heroTitulo}}
             h2.subtitle
-                | Este es un subtitulo descriptivo
+                | {{heroSubtitulo}}
 </template>
 
 <script>
-	
+export default {
+    data() {
+        return {
+            titulos:{
+                inicio: "Inicio",
+                devices: "Devices",
+                posts: "Post",
+                counter: "Contador",
+                diagnostico: "Diagnostico"
+            },
+            subtitulos: {
+                inicio: "Inicio de la Aplicacion",
+                devices: "Devices de la Aplicación",
+                posts: "Post de la Aplicación",
+                counter: "Contador de la Aplicación",
+                diagnostico: "Diagnostico de la Aplicación"
+            }
+        };
+    },
+    computed: {
+        heroTitulo () {
+            return this.titulos[this.$route.name] 
+        },
+        heroSubtitulo () {
+            return this.subtitulos[this.$route.name]
+        }
+    }
+};
 </script>
 
 <style lang="scss">
+
 </style>
